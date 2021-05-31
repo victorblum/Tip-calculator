e646905661e9fb6d5199b3753d5a52206f9e3516
Victor <victor@iMac-Sumbel.local>
2021-05-31 12:00:02 +0300

    final 2

--- script ENG.js
+++ script ENG.js
@@ -1,27 +1,28 @@
 
-let buttonCount = document.querySelector('.form__button_main');
-let buttonReset = document.querySelector('.form__button');
-let formInput = document.querySelectorAll('.form__input');
+const buttonCount = document.querySelector('.form__button_main');
+const buttonReset = document.querySelector('.form__button');
+const formInput = document.querySelectorAll('.form__input');
 
 let resultTip = document.querySelector('.results__value_tip');
 let resultTotal = document.querySelector('.results__value_total');
 
-let rusButton = document.querySelector('.up__button_rus');
+const rusButton = document.querySelector('.up__button_rus');
 
 buttonCount.addEventListener('click', clickButtonCount);
 buttonReset.addEventListener('click', clickButtonReset);
 rusButton.addEventListener('click', clickRusButton);
 
 function clickButtonCount() {
-    // IF === 0
-    // console.log(+formInput[0].value);
-    // console.log(+formInput[1].value);
-    // console.log(+formInput[2].value);
+    if (+formInput[0].value === 0 || +formInput[2].value === 0) {
+        resultTip.innerText = '___$';
+        resultTotal.innerText = '___$';
+    } else {
     let tipResult = (+formInput[0].value * (+formInput[1].value / 100)) / +formInput[2].value;
     let totalResult = (+formInput[0].value + (+formInput[0].value * (+formInput[1].value / 100))) / +formInput[2].value; 
         resultTip.innerText = `${Math.round(tipResult)}$`;
         resultTotal.innerText = `${Math.round(totalResult)}$`;
     }    
+}
 
 function clickButtonReset() {
     formInput[0].value = null;
@@ -34,6 +35,3 @@ function clickButtonReset() {
 function clickRusButton() {
     window.location.href = 'index.html';
 }
-
-//remove события функции написать в конце, let или const
-//getboodstrap.com это про адаптацию safari
--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 
-let buttonCount = document.querySelector('.form__button_main');
-let buttonReset = document.querySelector('.form__button');
-let formInput = document.querySelectorAll('.form__input');
+const buttonCount = document.querySelector('.form__button_main');
+const buttonReset = document.querySelector('.form__button');
+const formInput = document.querySelectorAll('.form__input');
 
 let resultTip = document.querySelector('.results__value_tip');
 let resultTotal = document.querySelector('.results__value_total');
 
-let engButton = document.querySelector('.up__button_eng');
+const engButton = document.querySelector('.up__button_eng');
 
 buttonReset.addEventListener('click', clickButtonReset);
 buttonCount.addEventListener('click', clickButtonCount);
 engButton.addEventListener('click', clickEngButton);
 
 function clickButtonCount() {
+    if (+formInput[0].value === 0 || +formInput[2].value === 0) {
+        resultTip.innerText = '___₽';
+        resultTotal.innerText = '___₽';
+    } else {
     let tipResult = (+formInput[0].value * (+formInput[1].value / 100)) / +formInput[2].value;
     let totalResult = (+formInput[0].value + (+formInput[0].value * (+formInput[1].value / 100))) / +formInput[2].value; 
         resultTip.innerText = `${Math.round(tipResult)}₽`;
         resultTotal.innerText = `${Math.round(totalResult)}₽`;
     }    
+}
 
 function clickButtonReset() {
     formInput[0].value = null;
